7f9a71bb8e70cad245876230ed110ebc75d9b2b2
Su Shi <1684739+metacpp@users.noreply.github.com>
2018-01-03 16:45:54 -0800

    Add environment setup bash script and update the readme.

--- README.md
+++ README.md
@@ -2,37 +2,37 @@
 
 This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
 
-# Terraform Module Generator
-`generator-terra-module` creates a base template to start a new terraform module.
+# Azure Terraform Module Generator
+`generator-az-terra-module` creates base template to start a new terraform module.
 
 ![Alt Text](/doc/scaffolding.640x434.gif)
 
 ## Prerequisites
 1. Install latest [Node LTS](https://nodejs.org).
 2. `npm install -g yo`
-3. `npm install -g generator-terra-module`
+3. `npm install -g generator-az-terra-module`
 
 ## Usage
 ```
-$yo terra-module
+$ yo az-terra-module
 ```
 *Note that this template will generate files in the current directory, so be sure always to run it against a new directory*
 
-### Setting Environment
+### Setting environment (Ruby)
 ```
-
+$ sudo ./tool/env_setup.sh
 ```
 
 ### Building module
 Once the project is scaffolded, at the root folder run:
 ```
-$bundle install
-$rake build
+$ bundle install
+$ rake build
 ```
 ### Running module
 At the root folder run:
 ```
-$terraform init
-$terraform plan
-$terraform apply
+$ terraform init
+$ terraform plan
+$ terraform apply
 ```
---
+++ tool/env_setup.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+set -e
+
+# Check if OS is supported.
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  TERRAFORM_OS_ARCH=linux_amd64
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  TERRAFORM_OS_ARCH=darwin_amd64
+else
+  echo "Unsupported Operating System: $OSTYPE" 1>&2
+  exit 1
+fi
+
+# Version, OS and Arch for Terraform.
+TERRAFORM_VERSION="0.11.1"
+# Version for Ruby SDK.
+RUBY_VERSION="2.3.3"
+RUBY_INSTALLED_VERSION_REGEX="^(ruby) ([0-9].[0-9].[0-9]p[0-9]+)"
+RBENV_INSTALLED_VERSION_REGEX="^(rbenv) ([0-9].[0-9].[0-9])"
+# Versions for gem packages.
+GEM_BUNDLER_VERSION="1.16.0"
+GEM_COLORIZE_VERSION="0.8.1"
+GEM_KITCHEN_TERRAFORM_VERSION="3.0.0"
+GEM_TEST_KITCHEN_VERSION="1.16.0"
+GEM_RAKE_VERSION="12.3.0"
+GEM_RSPEC_VERSION="3.7.0"
+
+# Install rbenv & ruby-build.
+if type rbenv &> /dev/null; then
+  echo "rbenv was previously installed."
+else
+  # Clean up legacy .rbenv
+  if [[ -d "$HOME/.rbenv" ]]; then
+    echo "Clean up legacy ~/.rbenv."
+    rm -rf "$HOME/.rbenv"
+  fi
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # rbenv.
+    apt-get update && \
+    apt-get install autoconf bison build-essential libssl-dev libyaml-dev \
+            libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 \
+            libgdbm-dev unzip
+    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+    export PATH="$HOME/.rbenv/bin:$PATH"
+    # ruby-build.
+    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  else # darwin*
+    # rbenv & ruby-build.
+    brew update && \
+    brew install rbenv ruby-build
+  fi
+fi
+
+# Initialize rbenv.
+eval "$(rbenv init -)"
+
+# Install Ruby SDK.
+# TODO: skip unneccessary install.
+rbenv install ${RUBY_VERSION} && rbenv global ${RUBY_VERSION}
+
+# Install Terraform runtime.
+curl -Os https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${TERRAFORM_OS_ARCH}.zip && \
+  curl -Os https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS && \
+  curl -s https://keybase.io/hashicorp/pgp_keys.asc | gpg --import && \
+  curl -Os https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig && \
+  gpg --verify terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig terraform_${TERRAFORM_VERSION}_SHA256SUMS && \
+  shasum -a 256 -c terraform_${TERRAFORM_VERSION}_SHA256SUMS 2>&1 | grep "${TERRAFORM_VERSION}_${TERRAFORM_OS_ARCH}.zip:\sOK" && \
+  unzip -o terraform_${TERRAFORM_VERSION}_${TERRAFORM_OS_ARCH}.zip -d /usr/local/bin
+# Cleanup downloaded files
+rm terraform_${TERRAFORM_VERSION}_${TERRAFORM_OS_ARCH}.zip
+rm terraform_${TERRAFORM_VERSION}_SHA256SUMS
+rm terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig
+
+# Install Gem packages.
+gem update --system && \
+  gem install bundler --no-document --version=${GEM_BUNDLER_VERSION} && \
+  gem install colorize --no-document --version=${GEM_COLORIZE} && \
+  gem install kitchen-terraform --no-document --version=${GEM_KITCHEN_TERRAFORM_VERSION} && \
+  gem install test-kitchen --no-document --version=${GEM_TEST_KITCHEN_VERSION} && \
+  gem install rake --no-document --version=${GEM_RAKE_VERSION} && \
+  gem install rspec --no-document --version=${GEM_RSPEC_VERSION}
+DOUBLE DEALS
+PROFILE
