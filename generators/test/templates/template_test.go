11760bfe8f45bbc7f0ec7c554a98c01e3a2adcfe
Yuping Wei <yupwei@microsoft.com>
2020-02-05 14:18:17 +0800

    update example

--- generators/test/templates/template_test.go
+++ generators/test/templates/template_test.go
@@ -1,17 +1,17 @@
 package test
 
 import (
-	"fmt"
-	"testing"
+  "fmt"
+  "testing"
 
-	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/gruntwork-io/terratest/modules/test-structure"
+  "github.com/gruntwork-io/terratest/modules/terraform"
+  "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
 func TestTerraformTemplate(t *testing.T) {
-	t.Parallel()
+  t.Parallel()
 
-	fixtureFolder := "./fixture"
+  fixtureFolder := "./fixture"
 
   // At the end of the test, clean up any resources that were created
   defer test_structure.RunTestStage(t, "teardown", func() {
@@ -19,40 +19,40 @@ func TestTerraformTemplate(t *testing.T) {
     terraform.Destroy(t, terraformOptions)
   })
 
-	// Deploy the example
-	test_structure.RunTestStage(t, "setup", func() {
-		terraformOptions := configureTerraformOptions(t, fixtureFolder)
+  // Deploy the example
+  test_structure.RunTestStage(t, "setup", func() {
+    terraformOptions := configureTerraformOptions(t, fixtureFolder)
 
-		// Save the options so later test stages can use them
-		test_structure.SaveTerraformOptions(t, fixtureFolder, terraformOptions)
+    // Save the options so later test stages can use them
+    test_structure.SaveTerraformOptions(t, fixtureFolder, terraformOptions)
 
-		// This will init and apply the resources and fail the test if there are any errors
-		terraform.InitAndApply(t, terraformOptions)
-	})
+    // This will init and apply the resources and fail the test if there are any errors
+    terraform.InitAndApply(t, terraformOptions)
+  })
 
-	// Check whether the length of output meets the requirement
-	test_structure.RunTestStage(t, "validate", func() {
-		terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
+  // Check whether the length of output meets the requirement
+  test_structure.RunTestStage(t, "validate", func() {
+    terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
 
-		stringList := terraform.Output(t, terraformOptions, "permutation_string_list_test")
-		const LENGTH int = 22
-		fmt.Println(stringList)
-		if len(stringList) != LENGTH {
-			t.Fatal("Wrong output")
-		}
-	})
+    stringList := terraform.Output(t, terraformOptions, "permutation_string_list_test")
+    const LENGTH int = 2
+    fmt.Println(stringList)
+    if len(stringList) != LENGTH {
+      t.Fatal("Wrong output")
+    }
+  })
 
 }
 
 func configureTerraformOptions(t *testing.T, fixtureFolder string) *terraform.Options {
 
-	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
-		TerraformDir: fixtureFolder,
+  terraformOptions := &terraform.Options{
+    // The path to where our Terraform code is located
+    TerraformDir: fixtureFolder,
 
-		// Variables to pass to our Terraform code using -var options
-		Vars: map[string]interface{}{},
-	}
+    // Variables to pass to our Terraform code using -var options
+    Vars: map[string]interface{}{},
+  }
 
-	return terraformOptions
+  return terraformOptions
 }
