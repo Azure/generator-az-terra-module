4939dfd3f2f7a533c3e7e2a9266f9e358e08f981
Yuping Wei <yupwei@microsoft.com>
2020-02-05 10:45:35 +0800

    version upgrade

--- generators/test/templates/template_test.go
+++ generators/test/templates/template_test.go
@@ -13,6 +13,12 @@ func TestTerraformTemplate(t *testing.T) {
 
 	fixtureFolder := "./fixture"
 
+  // At the end of the test, clean up any resources that were created
+  defer test_structure.RunTestStage(t, "teardown", func() {
+    terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
+    terraform.Destroy(t, terraformOptions)
+  })
+
 	// Deploy the example
 	test_structure.RunTestStage(t, "setup", func() {
 		terraformOptions := configureTerraformOptions(t, fixtureFolder)
@@ -34,12 +40,6 @@ func TestTerraformTemplate(t *testing.T) {
 		if len(stringList) != LENGTH {
 			t.Fatal("Wrong output")
 		}
-	})
-
-	// At the end of the test, clean up any resources that were created
-	test_structure.RunTestStage(t, "teardown", func() {
-		terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
-		terraform.Destroy(t, terraformOptions)
 	})
 
 }
